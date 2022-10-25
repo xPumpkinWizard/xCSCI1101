c83d3981b91cdefcc2554b5f6bb31745fc1a1cc8
Miguel Hernandez <miguel.hernandez13@utrgv.edu>
2022-10-25 12:57:15 -0500

    added random

--- hangman/main.py
+++ hangman/main.py
@@ -1,7 +1,21 @@
 import re
+import random
 
 #get the answer.
-answer = "What's up, doc?"
+pool_file = open("hangman-sample-answer-pool.txt")
+
+pool_answers = []
+
+pool_answer_line = pool_file.readline()
+
+while pool_answer_line : 
+    pool_answers.append(pool_answer_line)
+
+    pool_answer_line = pool_file.readline()
+
+pool_file.close()
+
+answer = random.choice(pool_answers)
 
 answer = answer.upper()
 
