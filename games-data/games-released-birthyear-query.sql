2d87559d0d6406fea2588d8f354beef71e5e54e5
xPumpkinWizard <hdzmike412@gmail.com>
2022-11-24 14:41:10 -0600

    sandbox

---
+++ games-data/games-released-birthyear-query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games WHERE first_release_date BETWEEN '2003-05-26 00:00:00' AND '2003-12-31 23:59:59' ORDER BY name;
---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (465496135, 0, 'HELLO');
+
+--UPDATE sandbox SET double_value = 1985 WHERE id = 215;
+
+--DELETE FROM sandbox WHERE id = 215;
+
+SELECT * FROM sandbox;
